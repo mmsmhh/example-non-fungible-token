f7d7803fd18df19fa2f5685a18a9ecd3085e92b2
Mahmoud Saleh <mmsmhh@gmail.com>
2020-09-04 20:07:44 +0200

    Update App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,77 +1,150 @@
-import React, { useState, useEffect } from 'react'
-import Computer from 'bitcoin-computer'
-import './App.css'
-import Card from './card'
-import Artwork from './artwork'
+import React, { useState, useEffect } from "react";
+import Computer from "bitcoin-computer";
+import "./App.css";
+import Loan from "./loan";
 
 function App() {
-  const [computer, setComputer] = useState(new Computer({
-    seed: 'title mercy exhibit wasp diesel tell state snow swamp benefit electric admit',
-    chain: 'BSV'
-  }))
-  const [balance, setBalance] = useState(0)
+  const [computer, setComputer] = useState(
+    new Computer({
+      seed:
+        "title mercy exhibit wasp diesel tell state snow swamp benefit electric admit",
+      chain: "BSV",
+    })
+  );
+  const [balance, setBalance] = useState(0);
 
-  const [title, setTitle] = useState('')
-  const [artist, setArtist] = useState('')
-  const [url, setUrl] = useState('')
+  const [amount, setAmount] = useState(0);
+  const [borrowerName, setBorrowerName] = useState("");
+  const [lenderName, setLenderName] = useState("");
 
-  const [revs, setRevs] = useState([])
-  const [artworks, setArtworks] = useState([])
-  const [refresh, setRefresh] = useState(0)
+  const [revs, setRevs] = useState([]);
+  const [loans, setLoans] = useState([]);
+  const [refresh, setRefresh] = useState(false);
+  const [loader, setLoader] = useState(false);
 
   useEffect(() => {
     const fetchRevs = async () => {
-      setBalance(await computer.db.wallet.getBalance())
-      setRevs(await computer.getRevs(computer.db.wallet.getPublicKey()))
-      setTimeout(() => setRefresh(refresh + 1), 3500)
-    }
-    fetchRevs()
-  }, [computer.db.wallet, refresh])
+      setBalance(await computer.db.wallet.getBalance());
+      setRevs(await computer.getRevs(computer.db.wallet.getPublicKey()));
+      setTimeout(() => setRefresh(!refresh), 5500);
+    };
+    fetchRevs();
+  }, [computer, computer.db.wallet, refresh]);
 
   useEffect(() => {
-    const fetchArtworks = async () => {
-      setArtworks(await Promise.all(revs.map(async rev => computer.sync(rev))))
-    }
-    fetchArtworks()
-  }, [revs, computer])
+    const fetchLoans = async () => {
+      await setLoans(
+        await (
+          await Promise.all(revs.map(async (rev) => computer.sync(rev)))
+        ).filter(
+          (loan) =>
+            loan.amount != null &&
+            loan.borrowerName != null &&
+            loan.lenderName != null
+        )
+      );
+    };
+    fetchLoans();
+  }, [revs, computer]);
 
-  useEffect(() => console.log('revs', revs), [revs])
-  useEffect(() => console.log('artworks', artworks), [artworks])
+  useEffect(() => {
+    if (loader) {
+      setLoader(false);
+      setAmount(0);
+      setBorrowerName("");
+      setLenderName("");
+    }
+  }, [loans]);
 
   const handleSubmit = async (evt) => {
-    evt.preventDefault()
-    const artwork = await computer.new(Artwork, [title, artist, url])
-    console.log('created artwork', artwork)
-  }
+    evt.preventDefault();
+    if (!(amount > 0)) {
+      alert("Please enter amount more than 0.");
+      return;
+    }
+
+    if (borrowerName.length < 1) {
+      alert("Borrower Name can't be empty.");
+      return;
+    }
+    if (lenderName.length < 1) {
+      alert("Lender Name can't be empty.");
+      return;
+    }
+
+    try {
+      await computer.new(Loan, [amount, borrowerName, lenderName]);
+      setLoader(true);
+    } catch (err) {
+      alert(err);
+    }
+    // console.log("created loan", loan);
+  };
 
   return (
     <div className="App">
       <h2>Wallet</h2>
-      <b>Address</b>&nbsp;{computer.db.wallet.getAddress().toString()}<br />
-      <b>Public Key</b>&nbsp;{computer.db.wallet.getPublicKey().toString()}<br />
-      <b>Balance</b>&nbsp;{balance/1e8} {computer.db.wallet.restClient.chain}<br />
+      <b>Address</b>&nbsp;{computer.db.wallet.getAddress().toString()}
+      <br />
+      <b>Public Key</b>&nbsp;{computer.db.wallet.getPublicKey().toString()}
+      <br />
+      <b>Balance</b>&nbsp;{balance / 1e8} {computer.db.wallet.restClient.chain}
+      <br />
       <button type="submit" onClick={() => setComputer(new Computer())}>
         Generate New Wallet
       </button>
-
-      <h2>Create new Artwork</h2>
+      <h2>Create new Loan</h2>
       <form onSubmit={handleSubmit}>
-        Title<br />
-        <input type="string" value={title} onChange={e => setTitle(e.target.value)} />
-
-        Artist<br />
-        <input type="string" value={artist} onChange={e => setArtist(e.target.value)} />
-
-        Url<br />
-        <input type="string" value={url} onChange={e => setUrl(e.target.value)} />
-
-        <button type="submit" value="Send Bitcoin">Create Artwork</button>
+        <b>Amount</b>
+        <br />
+        <input
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <b>Borrower Name</b>
+        <br />
+        <input
+          type="text"
+          value={borrowerName}
+          onChange={(e) => setBorrowerName(e.target.value)}
+        />
+        <b>Lender Name</b>
+        <br />
+        <input
+          type="text"
+          value={lenderName}
+          onChange={(e) => setLenderName(e.target.value)}
+        />
+        <button type="submit" value="Create loan" disabled={loader}>
+          Create Loan
+        </button>
       </form>
-
-      <h2>Your Artworks</h2>
-      <ul className="flex-container">
-        {artworks.map(artwork => <Card artwork={artwork} key={artwork.title + artwork.artist} />)}
-      </ul>
+      <h2>Your Loans</h2>
+      <div className="table">
+        {loader ? (
+          <div className="loader"></div>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>Amount</th>
+                <th>Borrower Name</th>
+                <th>Lender Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {loans.map((loan) => (
+                <tr key={loan.amount + loan.borrowerName + loan.lenderName}>
+                  <td>{loan.amount}</td>
+                  <td>{loan.borrowerName}</td>
+                  <td>{loan.lenderName}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </div>
   );
 }
